3a083e66074faf5d11eb8af143c898539aba62f8
k-gar <kgar@serverlesslaunchpad.com>
2025-12-20 12:51:53 -0800

    - added logo to other pages

--- web/src/features/Authentication/components/ConfirmSignUpForm.tsx
+++ web/src/features/Authentication/components/ConfirmSignUpForm.tsx
@@ -1,4 +1,4 @@
-import { TextInput, Button, Stack, Paper, Text, Center, Box, Group, Anchor, Input } from '@mantine/core';
+import { TextInput, Button, Stack, Paper, Text, Center, Box, Group, Anchor, Input, rem, Image } from '@mantine/core';
 import { useAuth } from '../../Authentication';
 import { useForm } from '@mantine/form';
 import { notifications } from '@mantine/notifications';
@@ -65,6 +65,12 @@ export const ConfirmSignUpForm = () => {
     return (
         <Center h="100vh">
             <Box w={500}>
+                <Image
+                    src="/svg/serverless_launchpad_logo.svg"
+                    alt="Serverless Launchpad Logo"
+                    style={{ height: rem(100) }}
+                    fit="contain"
+                />
                 <Paper radius="md" p="xl" withBorder>
                     <Text size="lg" fw={500} mb="md">
                         Confirm Your Sign Up
